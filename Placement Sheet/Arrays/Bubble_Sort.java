f9ef16d235a7592157e556b05bc2641f37f5923e
Samiran2004 <samirans170@gmail.com>
2025-06-24 09:20:13 +0530

    add/Bubble Sort[Link: https://www.naukri.com/code360/problems/bubble-sort_980524]

---
+++ Placement Sheet/Arrays/Bubble_Sort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bubble_Sort {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = input.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.printf("Enter %d number element of the array: ", i + 1);
+            arr[i] = input.nextInt();
+        }
+        System.out.println("Before sorting the array is: " + Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println("After sorting the array is : " + Arrays.toString(arr));
+    }
+
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}

e338db3b8de69433ee1278d5c47e1b7abaecf45b
Samiran2004 <samirans170@gmail.com>
2025-07-29 06:58:19 +0530

    update

--- Placement Sheet/Stack & Queue/Service Based/Implement_Stack_With_Linked_List/Main.java
+++ Placement Sheet/Stack & Queue/Service Based/Implement_Stack_With_Linked_List/Main.java
@@ -2,6 +2,14 @@ package Implement_Stack_With_Linked_List;
 
 public class Main {
     public static void main(String[] args) {
-
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        System.out.println("Top element: " + stack.top());
+        stack.pop();
+        System.out.println("Top Element: " + stack.top());
     }
 }

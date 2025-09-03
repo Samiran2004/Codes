9ad6070121274bac9f2f11edc59bd8e40824bea2
Samiran2004 <samirans170@gmail.com>
2025-09-03 22:48:45 +0530

    add/Encapsulation

--- Python/OOPs/ClassOOPs.py
+++ Python/OOPs/ClassOOPs.py
@@ -1,9 +1,10 @@
 class Student:
     numberOfStudents = 0
+    __schoolName = "DHHS"
     def __init__(self, name, roll_number, percentage):
         self.name = name
         self.roll_number = roll_number
-        self.percentage = percentage
+        self.__percentage = percentage
         Student.numberOfStudents += 1
 
     def say_hello(self):
@@ -16,21 +17,54 @@ class Student:
     def play(self):
         print("Study well, Now going to play!")
 
+    # Getter...
+    def getMarks(self):
+        return self.__percentage
+
+    # Setter...
+    def setMarks(self, newMarks):
+        self.__percentage = newMarks
+
+    @staticmethod
+    def getSchoolName():
+        return Student.__schoolName
+
+    def setNewScholName(self, newSchoolName, passCode):
+        if passCode == self.__auth():
+            Student.__schoolName = newSchoolName
+        else:
+            print("Error...")
+
+    def __auth(self):
+        return "0000"
+
 
 student1 = Student("Samiran", 42, 63.43)
 
-student1.say_hello()
-print(student1.roll_number)
-print(student1.percentage)
-student1.study()
+# student1.say_hello()
+# print(student1.roll_number)
+# print(student1.percentage)
+# student1.study()
 
 # print(id(student1))
 
 student2 = Student("Goku", 20, 50)
 
-student2.say_hello()
-print(student2.roll_number)
-print(student2.percentage)
-student2.play()
+# student2.say_hello()
+# print(student2.roll_number)
+# print(student2.percentage)
+# student2.play()
+#
+# print(student2.numberOfStudents)
 
-print(student2.numberOfStudents)
+
+"""
+    Encapsulation
+"""
+print(student1.getMarks())
+student1.setMarks(33.6)
+print(student1.getMarks())
+
+print(student1.getSchoolName())
+student1.setNewScholName("KTPP", "0000")
+print(student1.getSchoolName())
